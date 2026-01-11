42b2fe620c35f16043d140fe8bcb4e60d60a81ec
pr0_9 <pronaymondal500@gmail.com>
2026-01-11 20:44:40 +0530

    added outputs

---
+++ azure/outputs.tf
@@ -0,0 +1,22 @@
+output "aks_id" {
+  value = azurerm_kubernetes_cluster.aks.id
+}
+
+output "aks_fqdn" {
+  value = azurerm_kubernetes_cluster.aks.fqdn
+}
+
+output "aks_node_rg" {
+  value = azurerm_kubernetes_cluster.aks.node_resource_group
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
+  sensitive = true
+}
+
+resource "local_file" "kubeconfig" {
+  depends_on   = [azurerm_kubernetes_cluster.aks]
+  filename     = "kubeconfig"
+  content      = azurerm_kubernetes_cluster.aks.kube_config_raw
+}
