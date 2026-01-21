57c123aba17deb0bec0ca606370c196f094812b5
pr0_9 <pronaymondal500@gmail.com>
2026-01-21 12:34:20 +0530

    Updated variables.tf

--- aws/variables.tf
+++ aws/variables.tf
@@ -13,6 +13,31 @@ variable "cluster_name" {
   description = "EKS cluster name"
 }
 
+variable "ami_type" {
+  default = "AL2023_ARM_64_STANDARD"
+  description = "Amazon Machine Image type"
+}
+
+variable "instance_type" {
+  default = "t4g.small"
+  description = "Virtual Machine architecture type"
+}
+
+variable "min_size" {
+  default = 2 
+  description = "Minimum number of worker nodes"
+}
+
+variable "max_size" {
+  default = 4
+  description = "Maximum number of worker nodes"
+}
+
+variable "desired_size" {
+  default = 2 
+  description = "Desired number of worker nodes"
+}
+
 variable "endpoint_public_access_cidr" {
   default = "0.0.0.0/0"
   description = "add your public ip or cidr from which, wish to access the cluster"
